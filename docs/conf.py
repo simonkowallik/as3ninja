66557dc368526300366d8960279dc026356fbc2d
Simon Kowallik <github@simonkowallik.com>
2019-11-02 14:54:51 +0000

    docs/conf.py temporary remove sphinx_autodoc_typehints

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,11 @@ release = as3ninja.__version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc', 'sphinx_autodoc_typehints']
+extensions = ['sphinx.ext.todo',
+        'sphinx.ext.viewcode',
+        'sphinx.ext.autodoc',
+        ]
+#        'sphinx_autodoc_typehints',
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
