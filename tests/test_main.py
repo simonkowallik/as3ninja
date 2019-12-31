493f084602bc79972c2b2ec33387e5b1f3d1cdf2
Simon Kowallik <github@simonkowallik.com>
2019-12-31 16:42:45 +0100

    add test for __main__.py

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+import pytest
+import runpy
+
+
+def test_main(mocker):
+    """test as3ninja calls cli() on __main__"""
+    mocked_cli = mocker.patch("as3ninja.cli.cli")
+    runpy.run_module("as3ninja", run_name="__main__")
+    assert mocked_cli.called is True
